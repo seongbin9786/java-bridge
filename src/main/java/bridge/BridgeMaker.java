17d6833a6dbca26cc56fd6ee067530aabf82deb1
Seongbin Kim <seongbin9786@gmail.com>
2022-11-22 23:56:35 +0900

    feat(BridgeMaker): Implemented maker

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -18,6 +19,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        var answers = new LinkedList<String>();
+        for (int i = 0; i < size; i++) {
+            var upOrDown = this.bridgeNumberGenerator.generate();
+            var answer = this.markOf(upOrDown);
+            answers.add(answer);
+        }
+        return answers;
+    }
+
+    private String markOf(int upOrDown) {
+        if (upOrDown == 0) {
+            return "U";
+        }
+        return "D";
     }
 }
